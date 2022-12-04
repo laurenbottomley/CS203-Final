282e1c91d94eaad182202b8e90457a31a81fb7eb
esmondherry <esmondherry@gmail.com>
2022-12-04 00:21:02 -0500

    Add save and saveas

--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
@@ -61,7 +62,8 @@ public class TextEditor extends Application {
             stack.clear();
             stack2.clear();
         });
-
+        
+        SimpleStringProperty path = new SimpleStringProperty();
         // open handler
         open.setOnAction(e -> {
             FileChooser fileChooser = new FileChooser();
@@ -70,6 +72,7 @@ public class TextEditor extends Application {
             fileChooser.setTitle("Open Resource File");
             File file = fileChooser.showOpenDialog(primaryStage);
             if (file != null) {
+                path.setValue(file.getPath());
                 try {
                     Scanner in = new Scanner(file);
                     while (in.hasNext()) {
@@ -81,8 +84,49 @@ public class TextEditor extends Application {
                 }
             }
         });
-        save.setOnAction(e -> {
 
+        //save halder
+        save.setOnAction(e -> {
+            File file;
+            if (path.getValue() == null) {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.getExtensionFilters().add(new ExtensionFilter("Text file", "*.txt"));
+                fileChooser.setTitle("Save Resource File");
+                file = fileChooser.showSaveDialog(primaryStage);
+                path.set(file.getPath());
+            } else {
+                file = new File(path.getValue());
+            }
+
+            if (file != null) {
+                try {
+                    PrintWriter out = new PrintWriter(file);
+                    out.print(textArea.getText());
+                    out.close();
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+//saveas handler
+        saveAs.setOnAction(e -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"),
+                    new ExtensionFilter("All Files", "*.*"));
+            fileChooser.setTitle("Save Resource File");
+            File file = fileChooser.showSaveDialog(primaryStage);
+
+            if (file != null) {
+                path.set(file.getPath());
+                try {
+                    PrintWriter out = new PrintWriter(file);
+                    out.print(textArea.getText());
+                    out.close();
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+            }
         });
         exit.setOnAction(e -> {
             primaryStage.close();
